60c7accbca1e05163b1ab97e3085d9657c91c134
kenneth koontz <kenneth.koontz@gmail.com>
2012-08-25 22:13:38 +0000

    created production and dev envs

--- app.js
+++ app.js
@@ -5,7 +5,39 @@ var moment = require('moment');
 var ss = require('socketstream');
 var ea = require('everyauth');
 var request = require('request');
-var port = 3000;
+
+var oauthCreds = {
+  twitterProd: {
+    key: 'lWc6kG4NPaWYzoKf3M38Ag',
+    secret: 'at3gb0aWDbzqfIwph8iRnJLGZ37wxwWOLZMRt4Hk'
+  },
+  twitterDev: {
+    key: 'jVlsErvj6qv2UPQu5eYKw',
+    secret: 'wc1ihxtak7LRBjO2LjdECpt7DuwJ4UKnNoDlwTuIbQ'
+  },
+  facebookProd: {
+    key: '450449624976739',
+    secret: 'e856fdd60f0149e0ecc257914590c1e1'
+  },
+  facebooomDev: {
+    key: '521282271219601',
+    secret: ' 02c9461535e278081357ec65fe17861c'
+  }
+};
+
+if (ss.env === 'production') {
+  var port = 80;
+  var twitterKey = oauthCreds.twitterProd.key;
+  var twitterSecret = oauthCreds.twitterProd.secret;
+  var facebookKey = oauthCreds.facebookProd.key;
+  var facebookSecret = oauthCreds.facebookProd.secret;
+} else {
+  var port = 3000;
+  var twitterKey = oauthCreds.twitterDev.key;
+  var twitterSecret = oauthCreds.twitterDev.secret;
+  var facebookKey = oauthCreds.facebookDev.key;
+  var facebookSecret = oauthCreds.facebookDev.secret;
+}
 
 // Add to internal api.
 ss.api.add('db', db);
@@ -30,9 +62,17 @@ ss.http.route('/', function(req, res){
   res.serveClient('main');
 });
 
+var oauthCredentials = {
+    twitter: {
+    },
+    facebook: {
+
+    }
+};
+
 ea.twitter
-  .consumerKey('lWc6kG4NPaWYzoKf3M38Ag')
-  .consumerSecret('at3gb0aWDbzqfIwph8iRnJLGZ37wxwWOLZMRt4Hk')
+  .consumerKey(twitterKey)
+  .consumerSecret(twitterSecret)
   .findOrCreateUser(function(session, accessToken, accessTokenSecret, twitterUserMetadata) {
     session.services.twitter = twitterUserMetadata.screen_name;
     session.save();
@@ -41,8 +81,8 @@ ea.twitter
   .redirectPath('/')
 
 ea.facebook
-  .appId('450449624976739')
-  .appSecret('e856fdd60f0149e0ecc257914590c1e1')
+  .appId(facebookKey)
+  .appSecret(facebookSecret)
   .scope('read_stream') 
   .findOrCreateUser(function(session, accessToken, accessTokExtra, fbUserMetadata) {
     session.services.facebook = fbUserMetadata.username;
