03dab6d6bb7842e91499e74dc04cb21554f22ec1
kenneth koontz <kenneth.koontz@gmail.com>
2012-08-25 20:49:50 +0000

    fixed flashing image bug in intro

--- client/code/app/init.js
+++ client/code/app/init.js
@@ -1,7 +1,7 @@
 ss.rpc('auth.getSession', function(err, data) {
-  console.log(data);
   if (data.status === 'granted') {
     $('#account').html(ss.tmpl['navbar-account'].render({email: data.email, services: data.services})); 
+    $('#dashboard').html(ss.tmpl['home'].render());
     require('/services');
     $('#logout').bind('click', function() {
       ss.rpc('auth.logout', function(err, data) {
